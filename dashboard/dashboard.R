376f7a49e52c69cdc2b0745d8643ddc908a1c2e6
jokedurnez <joke.durnez@gmail.com>
2019-01-18 11:03:28 -0800

    add checkbuttons and make mult-var pairplots

--- dashboard/dashboard.R
+++ dashboard/dashboard.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(shinydashboard)
+library(GGally)
 
 source("pipelines/time_use_diary.R")
 
@@ -17,112 +18,167 @@ TUD_summarised <- TUD_preprocessed %>%
 
 numcols <- names(dplyr::select_if(TUD_summarised,is.numeric))
 
-ui <- dashboardPage(
+if (interactive()) {
   
-  skin = "black",
+  ui <- dashboardPage(
   
-  dashboardHeader(title = tags$a(href="http://www.openlattice.com", tags$img(src='logo.png',height=30))),
-  
-  dashboardSidebar(
-    sidebarMenu(
-      menuItem("Preprocessed data", tabName = "preprocessed"),
-      menuItem("Summary data", tabName = "summarised"),
-      menuItem("Visualisations", tabName = "visualisations")
-    )
-  ),
-  
-  dashboardBody(
-    tags$head(tags$link(rel="stylesheet", type="text/css", href="custom.css")),
-    tabItems(
-      tabItem(
-        "preprocessed",
-        fluidRow(
-          box(
-            width = 8, 
-            column(12, align="center", downloadButton("download_preprocessed", "Download"))
-          )
-        ),
-        fluidRow(
-          box(
-            width = 8, solidHeader = TRUE,
-            title = "Preprocessed data",
-            dataTableOutput(outputId = "preprocessed")
-          )
-        )
+    skin = "black",
+    
+    dashboardHeader(
+      title = tags$a(href="http://www.openlattice.com", div(img(src='logo.png',height=30), "CAFE analytics dashboard")),
+      titleWidth = 600
       ),
-      
-      tabItem(
-        "summarised",
-        fluidRow(
-          box(
-            width = 8, 
-            column(12, align="center", downloadButton("download_summarised", "Download"))
+    
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem("Preprocessed data", tabName = "preprocessed"),
+        menuItem("Summary data", tabName = "summarised"),
+        menuItem("Histograms", tabName = "histograms"),
+        menuItem("Cross-Plots", tabName = "cross-plots")
+      )
+    ),
+    
+    dashboardBody(
+      tags$head(
+        tags$link(rel="stylesheet", type="text/css", href="custom.css")
+        ),
+      tabItems(
+        tabItem(
+          "preprocessed",
+          fluidRow(
+            box(
+              width = 12, 
+              column(12, align="center", downloadButton("download_preprocessed", "Download"))
+            )
+          ),
+          fluidRow(
+            box(
+              width = 12, solidHeader = TRUE,
+              title = "Preprocessed data",
+              dataTableOutput(outputId = "preprocessed")
+            )
           )
         ),
-        fluidRow(
-          box(
-            width = 8, solidHeader = TRUE,
-            title = "Summarised data",
-            dataTableOutput(outputId = "summarised")
-          )
-        )
-      ),
-
-      tabItem(
-        "visualisations",
-        fluidRow(
-          box(
-            width = 8,
-            title = "Select column",
-            selectInput(inputId = 'columnname', choices = numcols, label='Column')
+        
+        tabItem(
+          "summarised",
+          fluidRow(
+            box(
+              width = 12, 
+              column(12, align="center", downloadButton("download_summarised", "Download"))
+            )
+          ),
+          fluidRow(
+            box(
+              width = 12, solidHeader = TRUE,
+              title = "Summarised data",
+              dataTableOutput(outputId = "summarised")
+            )
           )
         ),
-        fluidRow(
-          box(
-            width = 8, solidHeader = TRUE,
-            title = "Summarised data",
-            plotOutput(outputId = "histogram")
+  
+        tabItem(
+          "histograms",
+          fluidRow(
+            box(
+              width = 12,
+              title = "Select column",
+              selectInput(inputId = 'hist_column', choices = numcols, label='Column')
+            )
+          ),
+          fluidRow(
+            box(
+              width = 12, solidHeader = TRUE,
+              title = "Histogram",
+              plotOutput(outputId = "histogram")
+            )
+          )
+        ),
+        
+        
+        tabItem(
+          "cross-plots",
+          fluidRow(
+            column(
+              width = 4,
+              box(
+                width = 12,solidHeader = TRUE,
+                title = "Select column",
+                checkboxGroupInput(
+                  "cross_columns", 
+                  "Choose columns:",
+                  choiceNames = as.list(numcols),
+                  choiceValues = as.list(numcols)
+                )
+              )
+            ),
+            column(
+              width = 8,
+              box(
+                width = 12, solidHeader = TRUE,
+                title = "Cross-plot",
+                plotOutput("crossplot")
+              )
+            )
           )
         )
+        
       )
     )
   )
-)
+  
+  server <- function(input, output, session) {
+    
+    output$preprocessed <- renderDataTable({
+      as_tibble(TUD_preprocessed)
+    },
+    options = list(scrollX = TRUE)
+    )
+    
+    output$summarised <- renderDataTable({
+      as_tibble(TUD_summarised)
+    },
+    options = list(scrollX = TRUE)
+    )
+    
+    output$histogram <- renderPlot({
+      ggplot(
+        TUD_summarised,
+        aes_string(x = input$hist_column)
+      ) +
+        geom_histogram(
+          binwidth = 1,
+          fill = "#4c14c4"
+          )
+    })
+   
+    output$crossplot <- renderPlot({
+      if (length(input$cross_columns) > 0){
+        ggpairs(
+          TUD_summarised[,input$cross_columns],
+          color = "black",
+          diag = list(continuous = wrap("densityDiag", fill = "#4c14c4")),
+          lower = list(continuous=wrap("smooth", colour="#4c14c4"))
+        )
+      }
+    })
 
-server <- function(input, output, session) {
+    output$download_preprocessed <- downloadHandler(
+      filename = "CAFE_TUD_preprocessed.csv",
+      content = function(file) {
+        write.csv(TUD_preprocessed, file, row.names = FALSE)
+      }
+    )
   
-  output$preprocessed <- renderDataTable({
-    as_tibble(TUD_preprocessed)
-  },
-  options = list(scrollX = TRUE)
-  )
-  
-  output$summarised <- renderDataTable({
-    as_tibble(TUD_summarised)
-  },
-  options = list(scrollX = TRUE)
-  )
-  
-  output$histogram <- renderPlot({
-    TUD_summarised %>% pull(input$columnname) %>% hist()
-  })
-  
-  output$download_preprocessed <- downloadHandler(
-    filename = "CAFE_TUD_preprocessed.csv",
-    content = function(file) {
-      write.csv(TUD_preprocessed, file, row.names = FALSE)
-    }
-  )
-
-  output$download_summarised <- downloadHandler(
-    filename = "CAFE_TUD_summarised.csv",
-    content = function(file) {
-      write.csv(TUD_summarised, file, row.names = FALSE)
-    }
-  )
-  
-  
-  
+    output$download_summarised <- downloadHandler(
+      filename = "CAFE_TUD_summarised.csv",
+      content = function(file) {
+        write.csv(TUD_summarised, file, row.names = FALSE)
+      }
+    )
+    
+    
+    
+  }
 }
-
 shinyApp(ui, server)
