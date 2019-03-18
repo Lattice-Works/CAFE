f7e7875dc5d057d97634cf4820fa1e9f8cb89b45
jokedurnez <joke.durnez@gmail.com>
2019-03-18 09:15:36 -0700

    preintegration tud arguments

--- integration/preintegration_tud.R
+++ integration/preintegration_tud.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(lubridate)
 
-basedir = "/Users/jokedurnez/Box/CAFE Consortium/ParticipantIDs/"
-
-tud_file = paste0(basedir, "processed_datasets/TUD_v3_Combined.csv")
-tud_file_cleaned = paste0(basedir, "processed_datasets/TUD_v3_Combined_Cleaned.csv")
-participant_decoding_file = paste0(basedir, "participant_master/Participants_2019-03-05.csv")
-presurvey_file = paste0(basedir, "processed_datasets/Presurveys_Combined.csv")
+# filenames
+args <- commandArgs()
+tud_file <- args[6]
+tud_file_cleaned <- args[7]
+participant_decoding_file <- args[8]
+presurvey_file <- args[9]
 
 # read data
 TU <- read_csv(tud_file,
@@ -16,30 +16,6 @@ TU <- read_csv(tud_file,
     )
 )
 
-clean_id <- function(x){
-    if (x == "" | is.na(x)){return (NA)}
-    if (str_detect(x, "PM|Pm|pm")){
-
-        pid <- x %>% toupper() %>% str_replace("_", "-")
-
-        # if in perfect shape : return
-        if (!is.na(str_match(pid, "PM-\\d{2}-\\d{3,4}.{0,3}"))) {
-            return(pid)
-        }
-
-        # if PM00000
-        pidh <- pid %>% str_replace("_", "") %>% str_replace("-", "") %>% str_replace(" ","")
-        if (!is.na(str_match(pidh, "PM\\d{5}.*"))){
-            return(paste0("PM-", substr(pidh,3,5), "-", substr(pidh, 6, 7)))
-        }
-
-        return(pid)
-    }
-    return(x %>% toupper() %>% str_replace("_", "") %>% str_replace("-", ""))
-}
-
-TU$clean_ID = unlist(lapply(TU$PrimaryResponseID, clean_id))
-
 #######################################
 # FILTER TO ONE ENTRY PER DIARY ENTRY #
 #######################################
@@ -47,8 +23,8 @@ TU$clean_ID = unlist(lapply(TU$PrimaryResponseID, clean_id))
 # the following piece of code recodes the PID's
 codes <- read_csv(participant_decoding_file)
 TU <- TU %>%
-    left_join(codes, by = c(clean_ID = "tud_ID_TUD")) %>%
-    filter(!is.na(study_ID))
+    left_join(codes, by = c(PrimaryResponseID = "tud_id")) %>%
+    filter(!is.na(study_id))
 
 # this piece of code does the following:
 # - sort by action time
@@ -113,7 +89,7 @@ presurveys = read_csv(presurvey_file) %>% arrange(PrimaryResponseID, PS_StartDat
 
 groupers = names(TU)
 TU <- TU %>%
-  left_join(presurveys, all=TRUE, by=c('tud_ID_TTDP' = 'PrimaryResponseID')) %>%
+  left_join(presurveys, all=TRUE, by=c('tud_ps_id' = 'PrimaryResponseID')) %>%
   replace_na(list(PS_StartDate = as_datetime(0))) %>%
   mutate(counter = ymd_hms(PS_StartDate) < ymd_hms(StartDate)) %>%
   group_by_at(vars(one_of(groupers))) %>%
@@ -140,10 +116,10 @@ TUr = TU %>% filter(totalh2 > 26)
 TUleq = TU %>% filter(totalh2 <= 26)
 
 dates = TUr %>%
-    filter(!is.na(clean_ID)) %>%
+    filter(!is.na(clean_id)) %>%
     mutate(ActionDateString = format(ActionDate, format="%D")) %>%
-    select(clean_ID, ActionDateString, day) %>% unique() %>%
-    group_by(clean_ID, day) %>%
+    select(clean_id, ActionDateString, day) %>% unique() %>%
+    group_by(clean_id, day) %>%
     mutate(day_filled = 1:n()) %>% ungroup()
 
 TU = TU %>% left_join(dates) %>%
@@ -189,13 +165,18 @@ print(sum(counts4$totalh>24,na.rm=TRUE))
 # SPLIT > 24h over midnight #
 #############################
 
+TU <- TU %>% mutate(
+    startfixed = StartTime,
+    endfixed = EndTime
+)
+
 # TUmore24h = TU %>% group_by(PrimaryResponseID, day_filled, day, num_midnight) %>% filter(sum(duration/60)>26) %>% ungroup()
 # TUless24h = TU %>% group_by(PrimaryResponseID, day_filled, day, num_midnight) %>% filter(sum(duration/60)<=26) %>% ungroup() %>%
 #     mutate(
 #         startfixed = StartTime,
 #         endfixed = EndTime
 #     )
-# 
+#
 # TUmore24h <- TUmore24h %>%
 #   mutate(
 #     startfixed = ifelse(hm(StartTime)>hm(EndTime), paste(StartTime, "00:00", sep="; "), ""),
@@ -212,7 +193,7 @@ print(sum(counts4$totalh>24,na.rm=TRUE))
 #     endfixed = ifelse(endfixed=="", as.character(EndTime), endfixed),
 #     daysplit = as.integer(daysplit)
 #   ) %>% fill(daysplit) %>% replace_na(list(daysplit = 1))
-# 
+#
 # daysplit = TUmore24h$daysplit
 # TUmore24h$newday = 1
 # for (day in 2:(length(daysplit))){
@@ -230,9 +211,9 @@ print(sum(counts4$totalh>24,na.rm=TRUE))
 #     }
 #   }
 # }
-# 
+#
 # TU = bind_rows(TUmore24h, TUless24h)
-# 
+#
 # counts5 = TU %>% group_by(PrimaryResponseID, day, ActionDate, num_midnight, newday) %>% summarise(totalh = sum(duration/60))
 # print("SUBJECTS WITH 24h of data:")
 # print(sum(counts5$totalh==24,na.rm=TRUE))
